f3d4f737c66c0668c019ba6817fb6f128c0f5f19
Basawraj07 <basumat323@gmail.com>
2023-12-14 00:19:18 +0530

    tst

---
+++ langutil/Test.java
@@ -0,0 +1,10 @@
+import java.lang.Object;
+
+class Test extends Object{
+	public static void main(String[]args){
+		Test T = new Test();
+		System.out.println(T.toString());
+		System.out.println(T);
+	}
+	
+}
