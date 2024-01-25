e1bbf48aeb6ac94c58ff61c1122397ae88999a4f
Basawraj07 <basumat323@gmail.com>
2024-01-25 18:24:55 +0530

    'tst'

---
+++ EqualvsOp/Test3.java
@@ -0,0 +1,13 @@
+class Test3{
+	public static void main(String[]args){
+		String fname = "pro";
+		String lname = "pro";
+		String S1 = new String("pro");
+		
+		System.out.println(fname.equals(lname));
+		System.out.println(fname == lname);
+		System.out.println(fname == S1);
+		System.out.println(fname.equals(S1));
+		
+	}
+}
