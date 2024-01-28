9efaf2b6f5e7b6fa696e3cbfd3e3b555fa53c7e4
Basawraj07 <basumat323@gmail.com>
2024-01-29 00:31:41 +0530

    tst

---
+++ Arraylist/ArrayPract.java
@@ -0,0 +1,11 @@
+package ArrayPractEx;
+import java.util.ArrayList;
+public class ArrayPract{
+	public static void main(String[]args){
+		ArrayList al = new ArrayList();
+		al.add(100);
+		al.add("Basu");
+		al.add(300);
+		System.out.println(al);
+	}
+}
